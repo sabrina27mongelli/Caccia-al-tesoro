0782ff33aa6e66af81fc863a7466ef83c49c2162
sabrina27mongelli <162313047+sabrina27mongelli@users.noreply.github.com>
2024-04-09 13:20:05 +0200

    Update relazione.js

--- script/relazione.js
+++ script/relazione.js
@@ -1,1 +1,11 @@
+<script>
+function doLogin() {
+var username = document.getElementById ("username"). value;
+var password = document.getElementById ("password"). value;
+if (username = "" || password = "") {
+alert ("Username e password sono obbligatori");
+      ｝
+  }
+</script>
+
 
