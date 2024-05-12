34b790242846beb80874f98045bbb1a8709869e9
sabrina27mongelli <162313047+sabrina27mongelli@users.noreply.github.com>
2024-05-12 19:28:01 +0200

    Update Oggetto.js

--- script/Oggetto.js
+++ script/Oggetto.js
@@ -6,16 +6,16 @@ headers: {
       'Content-Type': "application/json',
 }
 body: JSON. stringify({
-      {"Nome": "Cane", "Esemplari presenti": 900000000, "Estinzione": No}
-      {"Nome": "Gatto", "Esemplari presenti": 600000000, "Estinzione": No}
-      {"Nome": "Elefante", "Esemplari presenti": 352271, "Estinzione": No}
-      {"Nome": "Giraffa", "Esemplari presenti": 117000, "Estinzione": No}
-      {"Nome": "Farfalla", "Esemplari presenti": 158000, "Estinzione": No}
-      {"Nome": "Ippopotamo", "Esemplari presenti": 130000, "Estinzione": No}
-      {"Nome": "Leone", "Esemplari presenti": 50000, "Estinzione": No}
-      {"Nome": "Aquila", "Esemplari presenti": 40000, "Estinzione": No}
-      {"Nome": "Mucca", "Esemplari presenti": 1500000, "Estinzione": No}
-      {"Nome": "Ghepardo", "Esemplari presenti": 6500, "Estinzione": No}
+      {"Nome": "Cane", "Esemplari presenti": 900000000, "Estinzione": "No"}
+      {"Nome": "Gatto", "Esemplari presenti": 600000000, "Estinzione": "No"}
+      {"Nome": "Orso polare", "Esemplari presenti": 352271, "Estinzione": "Si"}
+      {"Nome": "Panda", "Esemplari presenti": 117000, "Estinzione": "Si"}
+      {"Nome": "Farfalla", "Esemplari presenti": 158000, "Estinzione": "No"}
+      {"Nome": "Ippopotamo", "Esemplari presenti": 130000, "Estinzione": "No"}
+      {"Nome": "Koala", "Esemplari presenti": 50000, "Estinzione": "Si"}
+      {"Nome": "Aquila", "Esemplari presenti": 40000, "Estinzione": "No"}
+      {"Nome": "Mucca", "Esemplari presenti": 1500000, "Estinzione": "No"}
+      {"Nome": "Ghepardo", "Esemplari presenti": 6500, "Estinzione": "No"}
  })
 .then(response => response json()) // converte la risposta in JSON
 .then(data => console.log(data)) // stampa i dati
